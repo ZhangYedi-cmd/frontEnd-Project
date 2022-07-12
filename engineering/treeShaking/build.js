c4d51c0f5e9e8e43273990297ed498a329f7b1bc
ZhangYedi-cmd <178320369@qq.com>
2022-07-12 22:14:13 +0800

    tree shaking

--- engineering/treeShaking/build.js
+++ engineering/treeShaking/build.js
@@ -1,8 +1,20 @@
 const webpack = require("webpack"),
     path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
 
-
-const treeShaking = () => webpack({
+const f1 = () => webpack({
+    entry: './index.js',
+    output: {
+        filename: 'main.[contenthash].js',
+        path: path.resolve(__dirname, 'dist/treeShaking/'),
+        clean: true
+    },
+    mode: 'development',// 让webpack做标记，如果没有引用的模块就是unused 状态
+    optimization: {
+        usedExports: false,
+    },
+})
+const f2 = () => webpack({
     entry: './index.js',
     output: {
         filename: 'main.[contenthash].js',
@@ -11,10 +23,29 @@ const treeShaking = () => webpack({
     },
     mode: 'development',
     optimization: {
-        usedExports: false
-    }
+        usedExports: true,
+    },
 })
 
-treeShaking().run(() => {
+
+const f3 = () => webpack({
+    entry: './index.js',
+    output: {
+        filename: 'main.[contenthash].js',
+        path: path.resolve(__dirname, 'dist/treeShaking/'),
+        clean: true
+    },
+    optimization: {
+        usedExports: true,
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                test: /\.js(\?.*)?$/i,
+            }),
+        ],
+    },
+})
+
+f2().run(() => {
     console.log("build done");
 })
--- engineering/treeShaking/index.js
+++ engineering/treeShaking/index.js
@@ -1,3 +1,6 @@
 import {sum} from "./math";
+import user from "./user.json"
+import './global.js'
 
+console.log(user.name)
 console.log(sum(1, 3));
