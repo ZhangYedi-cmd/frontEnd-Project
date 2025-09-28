6d62d8663d280224f86896e512bc66c856ce4b5b
zhangyedi03 <zhangyedi03@meituan.com>
2025-09-28 16:26:34 +0800

    chore: 调整架构

---
+++ packages/mini-codes/bundle/build.js
@@ -0,0 +1,15 @@
+const webpack = require("webpack")
+const path = require("path");
+
+const f1 = () => webpack({
+  entry: './modules/esm-build-test/index.js',
+  mode: 'none',
+  output: {
+    filename: 'bundle-test.js',
+    path: path.resolve(__dirname, "dist/"),
+  }
+})
+
+f1().run(() => {
+  console.log("build done")
+})
